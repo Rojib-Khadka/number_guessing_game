8eb8c75ea66330daeef80f681993068a575bfdf3
Rojib Khadka <rojibkhadka@Rojibs-MacBook-Pro.local>
2025-04-21 15:17:09 +0545

    Remove unwanted files from GitHub

--- hello.py
+++
@@ -1,7 +0,0 @@
-def swap(x, y):
-    return y,x
-    a=5
-    b=6
-    a,b=swap(b,a)
-    print("a",a)
-    print("b",b)
--- javaa/excercise.java
+++
@@ -1,9 +0,0 @@
-public class excercise{
-    public static void main(String[] args) {
-        System.out.println("\nDivided by 3:");
-        for(int i=1;i<100;i++){
-            if(i%3==0)
-            System.out.println(i+",");
-        }
-    }
-}
